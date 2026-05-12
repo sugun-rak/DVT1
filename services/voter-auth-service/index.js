de3e98dcbb436c4c92b629676251980189b6cdf1
sugunr <sugun.rakshit@gmail.com>
2026-05-13 01:22:13 +0530

    Optimize Puppeteer memory usage to fix Render 503 crash during WhatsApp sync

--- services/voter-auth-service/index.js
+++ services/voter-auth-service/index.js
@@ -19,7 +19,16 @@ const whatsappClient = new Client({
     authStrategy: new LocalAuth({ clientId: "dvt1-voter-auth" }),
     puppeteer: {
         handleSIGINT: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
